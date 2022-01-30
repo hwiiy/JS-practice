232b38f49753defa64e3612c4007b1d37947b05d
hwiiy <ha5374@naver.com>
2022-01-30 17:27:55 +0900

    js : 반복문 for

--- main.js
+++ main.js
@@ -1,17 +1,13 @@
-console.log(typeof "Hello world");
-console.log(typeof 123);
-console.log(typeof true);
-console.log(typeof undefined);
-console.log(typeof null);
-console.log(typeof {});
-console.log(typeof []);
+// js 를 통해 html 제어하기
+const ulEl = document.querySelector("ul");
 
-function getType(data){
-    return Object.prototype.toString.call(data).slice(8,-1)
+for (let i = 0; i < 10; i++) {
+  const li = document.createElement("li");
+  li.textContent = `list-${i + 1}`;
+  if((i+1) %2 ===0){
+    li.addEventListener("click", function () {
+      console.log(li.textContent);
+    })
+  }
+  ulEl.appendChild(li);
 }
-
-console.log(getType(123))
-console.log(getType(false))
-console.log(getType(null))
-console.log(getType({}))
-console.log(getType([]))
